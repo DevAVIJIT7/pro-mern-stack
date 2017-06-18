6f09e3775fa99e1be9ddff6beb3ecc43e230f05b
Avijit Majhi <avijit.majhi@quantuminventions.com>
2017-06-18 21:36:48 +0530

    webpack

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import IssueAdd from './IssueAdd.js';
 const contentNode = document.getElementById('contents');
 
 class IssueFilter extends React.Component {
@@ -44,37 +45,6 @@ class IssueTable extends React.Component {
                 </thead>
                 <tbody>{issueRows}</tbody>
             </table>
-        );
-    }
-}
-
-class IssueAdd extends React.Component {
-    constructor() {
-        super();
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleSubmit(e) {
-        e.preventDefault();
-        var form = document.forms.issueAdd;
-        this.props.createIssue({
-            owner: form.owner.value,
-            title: form.title.value,
-            status: 'New',
-            created: new Date()
-        });
-        form.owner.value = ""; form.title.value = "";
-    }
-
-    render() {
-        return (
-            <div>
-                <form name="issueAdd" onSubmit={this.handleSubmit}>
-                  <input type="text" name="owner" placeholder="Owner" />
-                  <input type="text" name="title" placeholder="Title" />
-                  <button>Add</button>
-                </form>
-            </div>
         );
     }
 }
